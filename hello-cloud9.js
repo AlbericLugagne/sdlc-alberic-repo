c0c9d5671edcc1e5e8abf39fd25609af2b1591aa
Myazini <104372190+Myazini@users.noreply.github.com>
2022-04-29 12:24:36 +0200

    Update hello-cloud9.js
    Testing pull requests

--- hello-cloud9.js
+++ hello-cloud9.js
@@ -2,12 +2,12 @@ var readline = require('readline-sync');
 var i = 10;
 var input;
 
-console.log("Hello Cloud9!");
+console.log("Hello there, Alberic!");
 console.log("i is " + i);
 
 do {
-    input = readline.question("Enter a number (or 'q' to quit): ");
-    if (input === 'q') {
+    input = readline.question("Enter a number (or 'Exit' to quit): ");
+    if (input === 'Exit') {
         console.log('OK, exiting.');
     }
     else{
